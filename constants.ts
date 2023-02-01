1e9afbed510b3c0e8e7e067e617bbf4b1d1f9918
Johan Bové <jbove@deloitte.de>
2023-02-01 09:44:39 +0100

    version bump

--- constants.ts
+++ constants.ts
@@ -2,7 +2,7 @@ import { DateTime } from "npm:luxon@3";
 
 export const NAMESPACE = 'TimeKeeperV2';
 export const APPNAME = "timekeeper";
-export const VERSION = "1.6.0";
+export const VERSION = "1.8.0";
 export const DESCRIPTION = `
 This application gives you control over an Earthstar share.
 You can store any documents within the share.
@@ -12,11 +12,17 @@ When you first install the app you will need to do two things.
     1. Create a new Earthstar author or enter an existing author address and secret.
 
         $ deno run -A ./scripts/new_author.ts
+        
+        OR
+        
         $ deno run -A ./scripts/set_author.ts
 
     2. Create a new Earthstar share or enter an existing share address and secret.
 
         $ deno run -A ./scripts/new_share.ts
+
+        OR
+
         $ deno run -A ./scripts/add_share.ts
 
 When you restart the app after adding your credentials and share address you will get a menu of options.
