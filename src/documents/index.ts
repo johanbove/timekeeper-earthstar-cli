4e6d8803a4a11761fe87557dde4ee589ab667f27
Johan Bové <jbove@deloitte.de>
2023-02-08 14:06:10 +0100

    feat: adds ephemeral doc creation

--- src/documents/index.ts
+++ src/documents/index.ts
@@ -1,12 +1,13 @@
 import { NAMESPACE } from "../../constants.ts";
-import { Earthstar, Input, Table } from "../../deps.ts";
+import { Earthstar, Input, Table, Confirm } from "../../deps.ts";
 
 const settings = new Earthstar.SharedSettings({ namespace: NAMESPACE });
 
 export const edit = async (
-  opts: { replica: Earthstar.Replica; text?: string; docPath?: string },
+  opts: { replica: Earthstar.Replica; text?: string; docPath?: string; timestamp?: string },
 ) => {
-  let { replica, text, docPath } = opts;
+  let { replica, text, docPath, timestamp } = opts;
+  let deleteAfter;
 
   const allPaths = await replica.queryPaths();
 
@@ -23,10 +24,36 @@ export const edit = async (
     });
   }
 
+  if (docPath.indexOf('!') !== -1) {
+    const confirmed: boolean = await Confirm.prompt("Detected a ! in the docpath. Do you want to create an ephemeral doc?");
+
+    if (!confirmed) {
+      console.log('Ok. Then please start over.');
+      return;
+    }
+
+    timestamp = await Input.prompt({
+      message: "Enter a time and a date when this document should expire",
+      suggestions: allPaths,
+    });
+
+    const _date = new Date(timestamp);
+
+    if (!_date) {
+      throw new Error('That is an invalid date. Sorry.');
+    }
+
+    // time in micro seconds!
+    deleteAfter = _date.getTime() * 1000;
+
+    console.log(`This document will expire after ${new Date(deleteAfter / 1000)}.`)
+  }
+
   if (settings.author && text && docPath) {
     const result = await replica.set(settings.author, {
       path: docPath,
       text: text,
+      deleteAfter
     });
 
     if (Earthstar.isErr(result)) {
