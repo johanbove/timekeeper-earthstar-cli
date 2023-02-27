34d93a5551b4b1631fa3f407f99ff78073c26b27
Johan Bové <jbove@deloitte.de>
2023-02-27 18:24:02 +0100

    chore: bump version

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ import { DateTime } from "npm:luxon@3";
 export const LOCALE = "de-DE"; // Used for date formatting
 export const NAMESPACE = "TimeKeeperV2";
 export const APPNAME = "timekeeper";
-export const VERSION = "1.12.0";
+export const VERSION = "1.13.0";
 export const FORMAT_VERSION = "1.0";
 
 export const FORMAT_ABOUT_VERSION = "1.0";
